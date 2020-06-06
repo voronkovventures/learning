2e885f979428f3be3a29721d9f9458e6d6553e7c
Andrew Crower <46402168+voronkovventures@users.noreply.github.com>
2020-06-06 21:52:46 +0300

    Add files via upload

---
+++  script.js
@@ -0,0 +1,16 @@
+// начальное состояние кнопки — не кликнута
+var isClicked = false;
+// javascript здесь нашёл и выбрал элемент кнопки
+var button = document.getElementsByTagName('button')[0];
+
+// при клике по кнопке скрипт начинает выбирать
+button.onclick = function() {
+  if (isClicked) {
+    // если состояние кнопки — кликнута, делай это
+
+  } else {
+    // если состояние кнопки — не кликнута, делай это
+    button.classList.add('clicked');
+    isClicked = true;
+  }
+};
